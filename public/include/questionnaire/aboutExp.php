404deeec434625da7e0aab25cd3b281cef5381c5
SeviCeren <67870968+SeviCeren@users.noreply.github.com>
2020-07-08 15:21:45 +0200

    Update aboutExp.php
    Instead of 6 items about experiment, 2 items about social identification.
    I changed the first 2 items and deleted the rest.

--- public/include/questionnaire/aboutExp.php
+++ public/include/questionnaire/aboutExp.php
@@ -8,19 +8,16 @@
 
 include "../../../resources/config.php";
 
-if (sizeof($_POST) >= 5) {
+if (sizeof($_POST) >= 2) {
 
     $exp1 = $_POST["exp1"];
     $exp2 = $_POST["exp2"];
-    $exp3 = $_POST["exp3"];
-    $exp4 = $_POST["exp4"];
-    $exp5 = $_POST["exp5"];
-    $exp6 = $_POST["exp6"];
+
 
 
     $participant = $_GET['pid'];
 
-    $updateQuery = "UPDATE questionnaire SET exp1 = $exp1, exp2 = $exp2, exp3 = $exp3, exp4 = $exp4, exp5 = $exp5, exp6 = $exp6  WHERE pid = $participant";
+    $updateQuery = "UPDATE questionnaire SET exp1 = $exp1, exp2 = $exp2 WHERE pid = $participant";
     if (isset($connection)) {
         if ($connection->query($updateQuery)) {
             console_log("EXP data inserted successfully!");
@@ -51,7 +48,7 @@ else {
         else {
             console.log("Did not add " + element + " to the array, already in it!");
         }
-        validateAndActiateButton(6); //number of required items
+        validateAndActiateButton(2); //number of required items
     }
 
     function validateAndActiateButton(numberOfRequiredElements) {
@@ -66,7 +63,8 @@ else {
 
 <form method="post">
     <div class="item">
-        <p class="questionText"> 1. The timing of feedback on whether I was audited was unfair. (1 = do not agree at all, 7 = fully agree)
+        <p class="questionText"> 1. Being a member of the community of UK citizens is important to me. (1 = do not agree at all, 7 = agree completely)
+
         </p>
         <div class="radioDisplayHorizontal">
             <?php echo createLikert(7, "exp1"); ?>
@@ -74,48 +72,14 @@ else {
         </div>
     </div>
     <div class="item">
-        <p class="questionText"> 2. I was fairly treated by the tax authorities in the experiment. (1 = do not agree at all, 7 = fully agree)
+        <p class="questionText"> 2. . I feel a sense of pride in being a member of the community of UK citizens. (1 = do not agree at all, 7 = agree completely)
         </p>
         <div class="radioDisplayHorizontal">
             <?php echo createLikert(7, "exp2"); ?>
 
         </div>
     </div>
-    <div class="item">
-        <p class="questionText"> 3. During the experiment I felt fearful waiting for feedback whether I was audited or not. (1 = do not agree at all, 7 = fully agree)
-        </p>
-        <div class="radioDisplayHorizontal">
-            <?php echo createLikert(7, "exp3"); ?>
-
-        </div>
-    </div>
-
-    <div class="item">
-        <p class="questionText"> 4. During the experiment I felt angry waiting for feedback whether I was audited or not. (1 = do not agree at all, 7 = fully agree)
-        </p>
-        <div class="radioDisplayHorizontal">
-            <?php echo createLikert(7, "exp4"); ?>
-
-        </div>
-    </div>
-
-    <div class="item">
-        <p class="questionText"> 5. During the experiment, I felt uncertain about my decisions to pay my taxes honestly or not. (1 = do not agree at all, 7 = fully agree)
-        </p>
-        <div class="radioDisplayHorizontal">
-            <?php echo createLikert(7, "exp5"); ?>
-
-        </div>
-    </div>
-
-    <div class="item">
-        <p class="questionText"> 6. The probability of detection of tax evasion in the experiment was high. (1 = do not agree at all, 7 = fully agree)
-        </p>
-        <div class="radioDisplayHorizontal">
-            <?php echo createLikert(7, "exp6"); ?>
-
-        </div>
-    </div>
+   
 
     <input id="submitButton" type="submit" value="Next Page" disabled="disabled">
 
