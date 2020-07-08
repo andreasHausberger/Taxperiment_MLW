b324fde9b599bce5bbe7ff0311e56bd6f1ce790e
SeviCeren <67870968+SeviCeren@users.noreply.github.com>
2020-07-08 15:45:40 +0200

    Update numeracy.php
    I assume it should be num3 instead of num1?

--- public/include/questionnaire/numeracy.php
+++ public/include/questionnaire/numeracy.php
@@ -16,7 +16,7 @@ if (sizeof($_POST) >= 3) {
 
     $participant = $_GET["pid"];
 
-    $updateQuery = "UPDATE questionnaire SET num1 = $num1, num2 = $num2, num3 = $num1 WHERE pid = $participant";
+    $updateQuery = "UPDATE questionnaire SET num1 = $num1, num2 = $num2, num3 = $num3 WHERE pid = $participant";
 
     if (isset($connection)) {
         if ($connection->query($updateQuery)) {
