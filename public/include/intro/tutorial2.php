0ff06362052e83abd6e078dadea9126f9c98f04b
SeviCeren <67870968+SeviCeren@users.noreply.github.com>
2020-07-08 16:32:35 +0200

    Update tutorial2.php
    Made minor changes to the existing texts and included the last text ("we will know direct you to the informed consent.")

--- public/include/intro/tutorial2.php
+++ public/include/intro/tutorial2.php
@@ -4,26 +4,31 @@
         <span style="font-size:16px;"><span class="textSpan">Dear Participant,</span></span>
     </p>
     <p class="MsoNormal">
-        <span style="font-size:16px;"><span class="textSpan">Thank you for your interest in this study! This study is being conducted by the department of social psychology at Tilburg University. The intention of the study is to research and gain knowledge on financial decision making. The study will take about 30 minutes to complete. In return for your participation you will receive a payment that depends on your performance and decisions in the experiment, plus a show-up fee of 1 Euro.
+        <span style="font-size:16px;"><span class="textSpan">Thank you for your interest in this study! The study will take about 30 minutes to complete. In return for your participation, you will receive a guaranteed payment of £3.50. Additionally, you will receive a payment that depends on your decisions in the study. This additional payment can be as high as £4.00. This study does not include any deception. This means that you will not be presented with false  information.
+
 </span></span>
     </p>
     <p class="MsoNormal">
-        <span style="font-size:16px;"><span class="textSpan">This study consists of about 20 rounds. Each round starts with an effort task in which you will earn money based on your performance. After that you receive information about the tax due based on your income, and you have to make a decision about how much tax you pay. Your actual payment will be based on one of the rounds, which will be randomly selected.
+        <span style="font-size:16px;"><span class="textSpan">This study consists of about 20 rounds. Each round starts with an effort task in which you will earn money based on your performance. After that, you receive information about the tax due based on your income, and you have to decide how much tax you pay. Afterwards, we provide you with a questionnaire including questions related to the study and some demographic information. Your additional  payment at the end of the study will be based on one of the rounds, which is randomly selected.
+
 </span></span>
     </p>
     <p class="MsoNormal">
-        <span class="textSpan">Participation in this study is completely voluntary. You have the right to withdraw at any time or refuse to participate entirely. If you desire to withdraw, please simply close your internet browser. Risks are minimal for involvement in this study. It is very unlikely that answering these questions affects you emotionally or otherwise.</span>
+        <span class="textSpan">Participation in this study is completely voluntary and you have the right to decline to participate or to withdraw from the study once the participation has begun, without an explanation and without any negative consequences. If you desire to withdraw, please simply close your internet browser. Risks are minimal for involvement in this study. Your participation in this study does not include any physical or mental risks.</span>
     </p>
 
     <p class="MsoNormal">
-        <span class="textSpan">
-All data obtained from participants will be kept anonymous. There will be no record that links the data collected from you with any personal data from which you could be identified (e.g., your name, address, email, etc.). Once anonymized, these data may be made available to researchers via accessible data repositories and possibly used for novel purposes. The data will be stored for at least 10 years. </span>
+        <span class="textSpan">All data obtained from participants will be kept anonymous. There will be no record that links the data collected from you with any personal data from which you could be identified (e.g., your name, address, email, etc.). Once anonymized, these data may be made available to researchers via accessible data repositories and possibly used for novel purposes. The data will be stored for at least 10 years. 
+ </span>
     </p>
     <p class="MsoNormal">
-        <span xml:lang="en-us" lang="en-us"><span class="textSpan">This study has been approved by the Ethical Review Board (ERB).  If you have any remarks or complaints regarding this research, you may also contact the Ethics Review Board of Tilburg School of Social and Behavioral Sciences via <a
-                        href="mailto:erb@tilburguniversity.edu">ERB@tilburguniversity.edu</a>. If you have any further questions regarding this study, you may contact Christoph Kogler at <a
+        <span xml:lang="en-us" lang="en-us"><span class="textSpan">This study has been approved by the Ethical Review Board (ERB) of Tilburg University, The Netherlands.  If you have any remarks or complaints regarding this research, you may contact the Ethics Review Board of Tilburg School of Social and Behavioral Sciences via <a
+                        href="mailto:erb@tilburguniversity.edu">ERB@tilburguniversity.edu</a>. If you have any further questions or if you want to receive additional information, please contact the researcher: Christoph Kogler via <a
                         href="mailto:c.kogler@tilburguniversity.edu">c.kogler@tilburguniversity.edu</a>. </span></span>
     </p>
+    <p class="MsoNormal">
+        <span class="textSpan">We will now direct you to the informed consent. 
+ </span>
 </div>
 </body>
 </html>
