9a51cf319e12c6a27bd02a1afa61d2e1507c8a7f
SeviCeren <67870968+SeviCeren@users.noreply.github.com>
2020-08-29 14:47:35 +0300

    Update tutorial5.php
    Made changes within the texts. Also deleted the last sentence (it is now added to another sentence).

--- public/include/intro/tutorial5.php
+++ public/include/intro/tutorial5.php
@@ -4,16 +4,16 @@
 
             <br>
             <span class="textSpan">
-                The following situation is the basic situation:
+                This study looks at the tax compliance behavior.
 
             </span>
             <br>
             <span class="textSpan">
-                Each round you have a <b>guaranteed income of 1000 ECU </b>(experimental currency units). In addition, each round you can earn an <b>extra income</b> of up to <b>1000 ECU </b>based on your performance in an effort task. This results in a possible total income of 2000 ECU per round. Your task will be to indicate the amount of tax you decide to pay.
+                In the study you will face multiple decision rounds. For each round you receive a <b>guaranteed income of 1000 ECU </b>(experimental currency units). In addition, you can earn an <b>extra income</b> of up to <b>1000 ECU </b>based on your performance in an effort task. This results in a possible total income of 2000 ECU per round. You will then be required to pay taxes on this earned income.
             </span>
             <br>
             <span class="textSpan">
-                The <b>tax due</b>, your chance of getting checked (<b>audit probability</b>), and the potential <b>fine</b> for detected evasion will <b>vary</b> each round, so carefully read all the information.            <br>
+                The <b>tax due</b>, the <b>fine level</b>, and your <b>chance of getting audited</b> (chance of getting checked) will <b>vary</b> each round, so carefully read all the information. There are about 20 rounds.            <br>
             <span class="textSpan">
                 <br>
                 <i>Definitions: </i>
@@ -21,22 +21,19 @@
             </span>
             <br>
             <span class="textSpan">
-                Tax rate is the percentage of your income you have to pay as tax. It determines the <b>tax due</b>, the concrete amount you have to pay as tax.            </span>
+                Tax rate is the percentage of your income you are required to pay as tax. It determines the <b>tax due</b>, which is the actual amount you are asked to pay as tax.            </span>
             <br>
             <span class="textSpan">
-                An audit can take place to check whether you have paid all tax due. The <b>audit probability</b> is provided as a percentage. In case of an audit, if you did not pay enough tax you will have to pay back the missing amount and pay an additional fine (see below).
+                An audit can take place to check whether you have paid all tax due. The <b>audit probability</b> (chance of getting audited) is provided as a percentage. In case you completely paid the tax due, an audit is without any consequences. In case you have evaded tax and you are audited, you have to pay back the missing amount plus a fine (see below). Information on whether you have been audited and whether this audit results in a fine will be communicated after each round.
             </span>
             <br>
             <span class="textSpan">
-                Fine is what you have to pay in case you did not pay all of your tax due and get audited. The <b>fine</b> is determined based on how much of your tax due you evaded.
+                <b>Fine</b> is what you would have to pay in case you were audited and evaded tax. It consists of paying back the evaded tax plus an additional fine which is proportional to the evaded amount.
             </span>
             <br>
               </span>
             <br>
-            <span class="textSpan">
-                Information on whether you have been audited and whether this audit results in a fine will be communicated after each round.
-            </span>
-            <br>  
+            
         </p>
 
 
