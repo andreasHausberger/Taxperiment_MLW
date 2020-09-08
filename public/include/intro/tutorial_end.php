3620a2f940c4b3c42fc1af98a035cc5b39bac278
SeviCeren <67870968+SeviCeren@users.noreply.github.com>
2020-09-08 20:33:02 +0300

    Update tutorial_end.php
    Deleted the sentence that says the experiment starts on the next page since that is now not true. Added another sentence instead

--- public/include/intro/tutorial_end.php
+++ public/include/intro/tutorial_end.php
@@ -2,7 +2,7 @@
     <div class="contentContainer">
         <p>
             <span class="textSpan">
-                The examples are now finished. On the next page the experiment begins. Information on earned income, the tax rate, audit probability, and fine levels for each round will be displayed with MouselabWEB. This means that this information is hidden behind boxes. These boxes are labeled. To access the respective information, you have to move the mouse pointer over the box on the screen. As long as the pointer is over the box, it will display the information. Whenever the pointer moves out of the box, the box closes and the information is hidden again. See the example below and please make yourself familiar with how the boxes open and close.
+                The examples are now finished. You will soon make several tax decisions. For each round, information on earned income, the tax due, audit probability, and fine levels will be displayed with MouselabWEB. This means that this information is hidden behind boxes. These boxes are labeled. To access the respective information, you have to move the mouse pointer over the box on the screen. As long as the pointer is over the box, it will display the information. Whenever the pointer moves out of the box, the box closes and the information is hidden again. See the example below and please make yourself familiar with how the boxes open and close.
             </span>
             <br>
 
