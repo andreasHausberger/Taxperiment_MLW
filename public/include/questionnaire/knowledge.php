724f150e169120fc32da1f1e4e33c13b377f961a
Andreas Hausberger <hausbergerandreas1@gmail.com>
2020-11-09 21:05:55 +0100

    Knowledge: Reduced number of questions.

--- public/include/questionnaire/knowledge.php
+++ public/include/questionnaire/knowledge.php
@@ -1,31 +1,14 @@
 <?php
 $numberOfQuestions = 1;
-
-
-
 if(sizeof($_POST) >= $numberOfQuestions) {
 
-    $participant = $_GET['pid'];
+    global $db, $qb;
 
+    $participant = $_GET['pid'];
     $kno1 = postParamValue("kno1");
-    $kno2 = postParamValue("kno2");
-    $kno3 = postParamValue("kno3");
-    $kno4 = postParamValue("kno4");
-    $kno5 = postParamValue("kno5");
-    $kno6 = postParamValue("kno6");
-    $kno7 = postParamValue("kno7");
-    $kno8 = postParamValue("kno8");
-    $kno9 = postParamValue("kno9");
 
     $qb->addString("kno1", $kno1);
-    $qb->addString("kno2", $kno2);
-    $qb->addString("kno3", $kno3);
-    $qb->addString("kno4", $kno4);
-    $qb->addString("kno5", $kno5);
-    $qb->addString("kno6", $kno6);
-    $qb->addString("kno7", $kno7);
-    $qb->addString("kno8", $kno8);
-    $qb->addString("kno9", $kno9);
+
 
     $query = $qb->buildInsert("WHERE pid = $participant", true);
 
@@ -78,15 +61,6 @@ if(sizeof($_POST) >= $numberOfQuestions) {
             </div>
         </div>
     </div>
-    <div class="item">
-        <p class="questionText">
-        <b>
-            2. Please try to explain in your own words what an expected value is!
-        </b>
-        </p>
-        <div>
-            <input type="text" id="kno_input" name="kno2" style="width: 350px; height: 40px; margin-left: 12px;" onblur="addToArray('kno2')" value=" ">
-        </div>
-    </div>
+
     <input id="submitButton" type="submit" value="Next Page" disabled="disabled">
 </form>
