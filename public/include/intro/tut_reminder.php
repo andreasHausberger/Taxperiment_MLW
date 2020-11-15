9dfadddf1e5d7f3cd3d47df8060cd1653fd9d626
Martin Müller <martin.mueller82@univie.ac.at>
2020-11-15 14:46:25 +0100

    resolved minor issues #12

--- public/include/intro/tut_reminder.php
+++ public/include/intro/tut_reminder.php
@@ -9,11 +9,12 @@
     <div class="contentContainer">
         <p>
             <span class="textSpan">
-                Now, the actual experiment begins. <br>
-                Please keep in mind: Your task in every round is deciding whether to pay or not to pay the required taxes.
-                The relevant information will change in each round. There are ~20 rounds total. After the study ends,
-                one round will be chosen at random, and paid in GBP. Feedback whether you were audited will be shown 
-                after you completed all rounds.
+                The actual decisions will start on the next screen. Please keep in mind:<br>
+                <b>Your task in every round is deciding whether to pay or not to pay the tax due.
+                The displayed information will change in each round. Feedback on whether you have been audited will be shown 
+                after you completed all rounds.</b> 
+                <br>
+                There are about 20 rounds in total. After the study ends, one round will be chosen at random and paid in GBP. 
             </span>
             <br>
 
