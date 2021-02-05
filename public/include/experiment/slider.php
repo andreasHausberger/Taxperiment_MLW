98f50df15a5f18ef232a5fd4b1e68d2fa3aaa19e
Andreas Hausberger <hausbergerandreas1@gmail.com>
2021-02-05 16:21:05 +0100

    Timer. grr

--- public/include/experiment/slider.php
+++ public/include/experiment/slider.php
@@ -141,7 +141,7 @@ $nextString = "index.php?round=" . $_GET['round'] . "&mode=2&expid=$experimentID
 
     }
 
-    var time = 2;
+    var time = 20;
     document.getElementById("time").innerHTML = time;
 
     var countdownTimer = setInterval(function() {
