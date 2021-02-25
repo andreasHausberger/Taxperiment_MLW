5bc250aa07d1430ac053012f748f7c502199ed81
Andreas Hausberger <hausbergerandreas1@gmail.com>
2021-02-25 10:28:16 +0100

    Slider: Only shows "Time's Up" as Modal.

--- public/include/experiment/slider.php
+++ public/include/experiment/slider.php
@@ -11,9 +11,6 @@
                         <h5 class="modal-title" id="exampleModalLabel">Time's up!</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="modal-body">
-                        The timer has expired. All your completed sliders were counted.
-                    </div>
                 </div>
             </div>
         </div>
