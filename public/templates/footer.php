a889e3ed963b04d7e7d21a5516391d2d5ef1fc46
Andreas Hausberger <hausbergerandreas1@gmail.com>
2021-05-02 11:55:26 +0200

    Year

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -3,7 +3,7 @@
 <div id="footer">
     <hr style="width: 100%;">
     <p>
-       Taxperiment – 2020
+       Taxperiment – 2021
     </p>
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
